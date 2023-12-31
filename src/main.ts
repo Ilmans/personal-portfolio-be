7b272bb997d4a3affeca9c1c56f79972090bef75
Ilman Sunanuddin <ilmansunannudin2@gmail.com>
2023-12-31 19:17:50 +0700

    dinamic port

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 
 import { createDummyArticles } from "./app/helper";
 import { web } from "./app/web";
+require("dotenv").config();
 
 
 
 
 
-
-web.listen(3120,() : void => console.log(`Server run in port 3120`) )
+web.listen(process.env.PORT,() : void => console.log(`Server run in port 3120`) )
 
